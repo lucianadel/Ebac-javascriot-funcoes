f6c0e2d50cdd4c4bcc5639ae403d1c0453b507bf
Luciana <luciana342010@hotmail.com>
2024-04-10 12:45:23 -0300

    Exercicio, calcular area do retangulo

---
+++ node.js
@@ -0,0 +1,13 @@
+function calculaArea(base, altura) {
+    // Multiplica a base pela altura
+    const area = base * altura;
+
+    // Retorna o valor da área
+    return area;
+}
+
+// Exemplo de uso da função
+const areaRetangulo = calculaArea(5, 10);
+console.log("A área do retângulo é: ${areaRetangulo}"); 
+
+
